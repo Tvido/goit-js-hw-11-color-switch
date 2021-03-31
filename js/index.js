660a19452350586698f4d963a5a8b18468be9d70
Volodymyr Viter <mad.tvido@gmail.com>
2021-03-31 21:26:16 +0300

    upd

--- js/index.js
+++ js/index.js
@@ -1,6 +1,40 @@
-import colors from './colors';
-
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const maxNumber = colors.length-1;
+const startBtnRef = document.querySelector('[data-action=start]');
+const stopBtnRef = document.querySelector('[data-action=stop]');
+
+let timeoutId = null;
+let switchBg = false;
+
+const startBtnClick = () => {
+  if (switchBg) {
+    return;
+  }
+
+  switchBg = true;
+  
+  timeoutId = setInterval(() => {
+    const randomNum = randomIntegerFromInterval(0, maxNumber);
+    document.body.style.backgroundColor = colors[randomNum];
+  }, 1000);
+};
+
+const stopBtnClick = () => {
+  clearInterval(timeoutId);
+  switchBg = false;
+};
+
+startBtnRef.addEventListener('click', startBtnClick);
+stopBtnRef.addEventListener('click', stopBtnClick);
